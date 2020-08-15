e0d1cb9ae746e9b4ccc7c6239c8f54bf22fc12d7
ChanManChan <ngopal253@gmail.com>
2020-08-15 12:26:46 +0530

    transitions

--- 16-transitions/index.js
+++ 16-transitions/index.js
@@ -43,6 +43,9 @@ xAxisGroup
   .attr('text-anchor', 'end')
   .attr('fill', '#635F5D');
 
+
+const t500 = d3.transition().duration(500)
+
 const update = (data) => {
   y.domain([0, d3.max(data, d => d.orders)])
   x.domain(data.map(item => item.name))
@@ -51,21 +54,36 @@ const update = (data) => {
 
   rects.exit().remove()
 
-  rects
-    .attr('width', x.bandwidth)
-    .attr('height', (d) => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', (d) => x(d.name))
-    .attr('y', (d) => y(d.orders));
+  // update current shapes in DOM
+  // rects
+  // .attr('width', x.bandwidth)
+  // .attr('fill', 'orange')
+  // .attr('x', (d) => x(d.name))
+  // ending conditions
+  // .transition(t500)
+  // .attr('height', (d) => graphHeight - y(d.orders))
+  // .attr('y', (d) => y(d.orders));
 
+  // append the enter selection to the DOM
+  //! combining enter and update selection
   rects
     .enter()
     .append('rect')
-    .attr('width', x.bandwidth)
-    .attr('height', (d) => graphHeight - y(d.orders))
-    .attr('fill', 'orange')
+    // starting conditions
+    .attr('height', 0)
+    .attr('y', graphHeight)
+    .merge(rects)
+    // applying width to 0 is not necessary here before we are already doing that in the tween
+    // .attr('width', 0)
     .attr('x', (d) => x(d.name))
-    .attr('y', (d) => y(d.orders));
+    .attr('fill', 'orange')
+    // ending conditions
+    .transition(t500)
+    .attrTween('width', widthTween)
+    .attr('height', (d) => graphHeight - y(d.orders))
+    .attr('y', (d) => y(d.orders))
+
+  // anything below .merge() applies to both the enter selection and the current selection already in the DOM. The stuff above the .merge() is applied only to the enter selection
 
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
@@ -99,3 +117,28 @@ db.collection('dishes').onSnapshot(res => {
   update(data)
 
 })
+
+
+// TWEENS
+
+const widthTween = d => {
+  // define interpolation
+  // d3.interpolate returns a function which we call 'i'
+
+  let i = d3.interpolate(0, x.bandwidth())
+
+  // return a function which takes in a time ticker 't'
+  return function (t) {
+    // return the value from passing the ticker into the interpolation
+    return i(t)
+  }
+}
+
+
+// Starting conditions:
+// Y = graphHeight
+// Height = 0
+
+// Ending conditions:
+// Y = y(d.orders)
+// Height = graphHeight - y(d.orders)
